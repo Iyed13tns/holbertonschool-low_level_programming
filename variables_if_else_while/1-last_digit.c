f39d02bbe43c2add44c8304d3f17a74cfcd9a012
Name <m@g.com>
2024-10-24 15:37:26 +0200

    holberton

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,28 +1,31 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
+/* betty style doc for function main goes there */
 /**
-* main — print the last digit of the number stored in the variable n.
-*Return:0
-**/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int n, last_digit;
-srand(time(0));
-n = rand() — RAND_MAX / 2;
-last_digit = n % 10;
-printf(“Last digit of %d”, n, last_digit);
-if (last_digit > 5)
-{
-puts(“ and is greater than 5”);
-}
-else if (last_digit == 0)
-{
-puts(“ and is 0”);
-}
-else
-{
-puts(“ and is less than 6 and not 0”);
-}
-return(0);
+       int n ;
+       srand(time(0));
+       n = rand() — RAND_MAX / 2;
+       last_digit = n % 10;
+              printf(“Last digit of %d”, n, last_digit);
+              if (last_digit > 5)
+              {   
+              puts(“ and is greater than 5”);
+              } 
+              else if (last_digit == 0)
+              { 
+              puts(“ and is 0”);
+              }
+              else
+              {
+              puts(“ and is less than 6 and not 0”);
+              }
+              return(0);
 }
