1ff76552d006db8ce5dadc4096d87c1218f6f67b
Name <m@g.com>
2024-10-24 19:22:25 +0200

    holberton

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,19 +13,13 @@ int main(void)
        int n ;
        srand(time(0));
        n = rand() — RAND_MAX / 2;
-       last_digit = n % 10;
-              printf(“Last digit of %d”, n, last_digit);
-              if (last_digit > 5)
-              {   
-              puts(“ and is greater than 5”);
-              } 
-              else if (last_digit == 0)
-              { 
-              puts(“ and is 0”);
-              }
-              else
-              {
-              puts(“ and is less than 6 and not 0”);
-              }
-              return(0);
+       last = n % 10;
+
+       if (last == 0)
+        printf("Last digit of %d is %d ans is 0\n" , n, last);
+       else if  (last > 5)
+        prinft("Last digit of %d is %d and is greater than 5\n", n, last);
+       else
+        printf("Last digit of %d is less than 6 and not 0\n", n, last);
+       return (0);
 }
