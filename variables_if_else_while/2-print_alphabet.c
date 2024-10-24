76653bd2c3b1f83f9a9e5d50a598e1c94e21ef02
Name <m@g.com>
2024-10-24 21:42:56 +0200

    holberton

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,15 @@
  */
 int main(void)
 {
-for(int x = 'A'; x <= 'Z'; x++)
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+	
 {
-x = tolower(x);
 putchar(x);
 }
-return 0;
+putchar('\n');
+
+return (0);
 }
+
