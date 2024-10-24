d72b25149bf606dce2272ccb1b79195858cc9e73
Name <m@g.com>
2024-10-24 20:52:59 +0200

    j

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,6 +21,5 @@ else if (r == 0)
 printf("Last digit of %d is %d and is 0\n", n, r);
 else if (r < 6)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, r);
-       
 return (0);
 }
