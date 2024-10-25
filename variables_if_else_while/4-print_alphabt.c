4f80f537da728a5758e7732c67686404dc70e1c3
Name <m@g.com>
2024-10-25 10:33:43 +0200

    holberton

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
- int letter;
+int letter;
 
- for (letter = 'a'; letter <= 'z'; letter++)
- {
-  if (letter == 'q' || letter == 'e')
-  {
-   continue;
-  } 
-  putchar(letter);
- }
- putchar('\n');
- return (0);
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter == 'q' || letter == 'e')
+{
+continue;
+} 
+
+putchar(letter);
+}
+putchar('\n');
+return (0);
 }
