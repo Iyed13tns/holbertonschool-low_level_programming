a1fd8115c4fee5f9a4e5c84073fc8454e1d4c4f5
Name <m@g.com>
2024-10-25 11:13:23 +0200

    holbertopn

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-    int digit;
+int digit;
 
-    for (digit = '0'; digit <= '9'; digit++)
-    {
-        putchar(digit);
-    }
-    putchar('\n');
-    return (0);
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+}
+putchar('\n');
+return (0);
 }
 
