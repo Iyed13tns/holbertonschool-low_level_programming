71501b30ee3929431547f5ddf48e6d00dfd4f20c
Name <m@g.com>
2024-10-25 11:33:31 +0200

    holberton

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -8,12 +8,12 @@
  */
 int main(void)
 {
- int letter;
+int letter;
 
- for (letter = 'z'; letter >= 'a'; letter--)
- {
-  putchar(letter);
- }
- putchar('\n');
- return (0);
+for (letter = 'z'; letter >= 'a'; letter--)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
 }
