91915358b0d16190047dcf0d91dadf9c8115c9dc
Name <m@g.com>
2024-10-25 11:07:26 +0200

    holberton

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <ctype.h>
+
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-int digit;
- 
-for (digit = '0'; digit <= '9'; digit++)
-{
-putchar(digit);
+    int digit;
+
+    for (digit = '0'; digit <= '9'; digit++)
+    {
+        putchar(digit);
+    }
+    putchar('\n');
+    return (0);
 }
-putchar('\n');
-return (0);
+
