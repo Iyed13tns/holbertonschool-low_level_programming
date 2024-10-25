65c7279573694e877b94e70189e94dbe17b383ce
Name <m@g.com>
2024-10-25 10:30:42 +0200

    holbert

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, then in uppercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+ int letter;
+
+ for (letter = 'a'; letter <= 'z'; letter++)
+ {
+  if (letter == 'q' || letter == 'e')
+  {
+   continue;
+  } 
+  putchar(letter);
+ }
+ putchar('\n');
+ return (0);
+}
