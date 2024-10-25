977981d06688001b3263617a7ba946c1fa555c14
Name <m@g.com>
2024-10-25 13:24:04 +0200

    holberton

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -7,16 +7,17 @@
  */
 int main(void)
 {
- int digit;
+    int digit;
  
- for (digit = '0'; digit <= '9'; digit++)
- {
-   putchar(digit);
-   for(digit = 'a'; digit <= 'f'; digit++)
-   {
+    for (digit = '0'; digit <= '9'; digit++)
+    {
      putchar(digit);
-}
+    }
+    for(digit = 'a'; digit <= 'f'; digit++)
+    {
+       putchar(digit);
+    }
 
-putchar('\n');
-return (0);
+ putchar('\n');
+ return (0);
 }
