bb40d64bfe0f754a4bc6de3b19c767ec9ee9b865
Name <m@g.com>
2024-10-27 15:15:49 +0100

    holberton

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * _islowe - Checks for lowercase character
+ * @C: The ccharacter to be checked
+ * Return: 1 for lowercase character or 0 anything else
  */
 
 int _islower(int c)
