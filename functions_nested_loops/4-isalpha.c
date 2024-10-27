54e32f810d26cd1c72bcfed1c03487ff54119ae2
Name <m@g.com>
2024-10-27 21:43:23 +0100

    holberton

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
-isalpha - Checks for alphabetic character
+* _isalpha - Checks for alphabetic character
 * @c: The character to be checked
+*
 * Return: 1 for alphabetic character or @ for anything else
 */
 int _isalpha (int c)
