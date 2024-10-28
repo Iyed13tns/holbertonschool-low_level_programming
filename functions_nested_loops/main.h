c35778fa80333cbab48743ce4938a6ab45520b9b
Name <m@g.com>
2024-10-28 14:37:17 +0100

    holberton

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);  // Déclaration de _putchar
-int _isalpha(int c);   // Déclaration de _isalpha
+int _putchar(char c); /* Déclaration de _putchar */
+int _isalpha(int c);  /* Déclaration de _isalpha */
 
 #endif /* MAIN_H */
-
