36e2c6a7be5d40b077683b4e74ace9c600005684
Name <m@g.com>
2024-10-29 00:08:28 +0100

    holberton

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -8,12 +8,12 @@
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return (1); /* Return 1 if c is uppercase */
-    }
-    else
-    {
-        return (0); /* Return 0 otherwise */
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
     }
 }
