20a22279efc94688a18119fad24de7ab1bf412b5
Name <m@g.com>
2024-10-29 00:09:55 +0100

    holberton

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - checks if a character is uppercase
  * @c: the character to check
- * 
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
@@ -15,5 +15,5 @@ return (1);
 else
 {
 return (0);
-    }
+}
 }
