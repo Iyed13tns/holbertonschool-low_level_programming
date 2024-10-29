e1141518ebebd6dc8e9e676ea83474eae014d58b
Name <m@g.com>
2024-10-29 11:02:16 +0100

    holberton

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,14 @@
  */
 void print_most_numbers(void);
 
-   int(c)
+  int(c)
 
-   for (c = '0'; c <= '9'; c++)
-   { 
-       if ( c !=2 && c !=2 )
-   
+  for (c = '0'; c <= '9'; c++)
+  { 
+      if ( c !=2 && c !=2 )
+      {
        _putchar(c);
-   }
-     _putchar('\n')
+      }
+  } 
+  _putchar('\n')
 }
