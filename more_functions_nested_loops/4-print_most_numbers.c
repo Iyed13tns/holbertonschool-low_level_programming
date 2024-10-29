936d0d4a6352bd964ff8393aae19f57d09c171be
Name <m@g.com>
2024-10-29 09:57:34 +0100

    holberton

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print numbers - from 0 to 9
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void);
+{
+   int(c)
+   for (c = '0'; c <= '9'; c++)
+   if ( c !=2 && c !=2 )
+   {
+       _putchar(c);
+   }
+   _putchar('n\')
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-int _putchar(char c);
-void print_numbers(void);
-
-#endif /* MAIN_H */
-
+*
+!*.c
+*-main.c
+_putchar.c
