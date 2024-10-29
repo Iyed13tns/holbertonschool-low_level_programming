722308aff67a71323f30028503fcea769974208a
Name <m@g.com>
2024-10-29 15:32:51 +0100

    holberton

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print numbers - from 0 to 9
+ * print_most_numbers - print from 0 to 9
  *
  * Return: Always 0.
  */
@@ -10,7 +10,7 @@ void print_most_numbers(void)
 {
 	int c;
 
-	for (c = 0; c <= 9; c++)
+	for (c = 48; c <= 57; c++)
 	{ 
 		if (c != 2 && c != 4)
 			_putchar(c);
