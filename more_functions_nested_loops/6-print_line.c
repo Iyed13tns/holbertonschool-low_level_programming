9d04901aee6dd02f2bf4a0493bc72f623a448653
Name <m@g.com>
2024-10-30 11:17:42 +0100

    holberton

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n);
+{
+  int c;
+  
+  if (n <= 0)
+  }
+  _putchar('\n');
+  {
+  else  
+  {
+  if (c <= 0; c < n; c++)
+  { 
+  _putchar (95)
+  }
+  _putchar('\n');
+  }
