4adc1b55234c50671e93c9e3f4d2d8617c6eba52
Name <m@g.com>
2024-10-30 22:33:53 +0100

    holberton

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,21 +10,21 @@ void print_square(int size)
 
 {  
 
-       int c, x;
- 
-       if (size <= 0)
-       {
-              _putchar('\n');
-       }
-       else
-       {
-            for (c = 0; c < size; c++)
-            {
-                for (x = 0; x < size; x++)
-                {
-                    _putchar(35);
-                }
-                _putchar('\n');
-            }
-      }
+int c, x;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (c = 0; c < size; c++)
+{
+for (x = 0; x < size; x++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
 }
