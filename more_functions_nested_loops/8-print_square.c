b4f803d20cc988d343d6a0128121a181d87203d8
Name <m@g.com>
2024-10-30 22:31:43 +0100

    holberton

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,30 @@
 #include "main.h"
 
 /**
- * print_diagonal - function that draws a diagonal line
+ * print_square - function that prints a square
  * @size: The number of times to print diagonal lines
- * Return: Always 0.
+ * Return: empty
  */
+
+void print_square(int size)
+
+{  
+
+       int c, x;
+ 
+       if (size <= 0)
+       {
+              _putchar('\n');
+       }
+       else
+       {
+            for (c = 0; c < size; c++)
+            {
+                for (x = 0; x < size; x++)
+                {
+                    _putchar(35);
+                }
+                _putchar('\n');
+            }
+      }
+}
