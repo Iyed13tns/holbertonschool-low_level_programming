e713f2e0dd835e0cc6af3c02d54d1c6aaeb6614d
Name <m@g.com>
2024-10-30 19:19:30 +0100

    holberton

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -11,19 +11,19 @@ void print_line(int n)
 {
 
 
-   int c = 0;
+   int c;
 
-   if ('n' <= 0)
+   if (n <= 0)
    {
       _putchar('\n');
    }
    else
    {  
-   for (c = 0; c < n; c++)
-   {
-      _putchar (95);
+       for (c = 0; c < n; c++)
+      {
+           _putchar (95);
+      }
+       _putchar('\n');
+
    }
-   _putchar('\n');
-
-
 }
