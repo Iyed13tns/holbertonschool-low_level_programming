9684c37ad17d129b413c07d87742739886cb8fcb
Name <m@g.com>
2024-10-31 14:37:06 +0100

    holberton

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Print a Fizz Buzz program
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+        int x;
+        for (x = 1; x <= 100; x++)
+        {
+        if ((x % 3 == 0) && (x % 5 == 0))
