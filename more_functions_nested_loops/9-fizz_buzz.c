9c9e4ab973f34d44bc9bf2e4ba738606fccc73e0
Name <m@g.com>
2024-10-31 15:45:00 +0100

    holberton

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,27 +11,28 @@ int main(void)
         int x;
         for (x = 1; x <= 100; x++)
         {
-        if ((x % 3 == 0) && (x % 5 == 0))
-        {
-        printf("FizzBuzz");
+             if ((x % 3 == 0) && (x % 5 == 0))
+             {
+                printf("FizzBuzz");
+             }
+             else if (x % 3 == 0)
+             {
+                print("Fizz");
+             }
+             else if (x % 5 == 0)
+             {
+                printf("Buzz");
+             }
+             else
+             {
+                printf("%d", x);
+             }
+             if (x != 100)
+             {
+                printf(" ");
+             }
         }
-        else if (x % 3 == 0)
-        {
-        print("Fizz");
-        }
-        else if (x % 5 == 0)
-        {
-        printf("Buzz");
-        }
-        else
-        {
-        printf("%d", x);
-        }
-        if (x != 100)
-        {
-        printf(" ")
-        }
-        }
+       
         printf("\n");
         return (0);
 }
