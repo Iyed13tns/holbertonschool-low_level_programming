e3495e781449ce46850e597823002c0994053dbc
Name <m@g.com>
2024-11-01 17:55:24 +0100

    holberton

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - Print a Fizz Buzz program
@@ -9,30 +10,30 @@ int main(void)
 
 {
         int x;
-        for (x = 1; x <= 100; x++)
+        
+        for (x= 1; x <= 100; x++)
         {
-             if ((x % 3 == 0) && (x % 5 == 0))
-             {
-                printf("FizzBuzz");
-             }
-             else if (x % 3 == 0)
-             {
-                printf("Fizz");
-             }
-             else if (x % 5 == 0)
-             {
-                printf("Buzz");
-             }
-             else
-             {
-                printf("%d", x);
-             }
-             if (x != 100)
-             {
-                printf(" ");
-             }
+           if ((x % 3 == 0) && (x % 5 == 0))
+           {
+                 printf("FizzBuzz");
+           }
+           else if (x % 3 == 0)
+           {
+                 printf("Fizz");
+           }
+           else if (x % 5 == 0)
+           {
+                 printf("Buzz");
+           }
+           else
+           {
+                 printf("%d", x);
+           }
+           if (x != 100)
+           {
+                 printf(" ");
+           }
         }
-       
         printf("\n");
         return (0);
 }
