428e8afe0812e3e2db389b08de4b73670d92488c
Name <m@g.com>
2024-11-01 18:44:52 +0100

    holberton

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,3 +16,19 @@ void print_triangle(int size)
       {
       _putchar('\n');
       }
+      else
+      {
+      for (x = 0; x < size; x++)
+      {
+      for (y = size - x; y > 1; y--)
+      {
+      _putchar(32);
+      }
+      for (z = ; z <= x; z++)
+      {
+      _putchar(35);
+      }
+      _putcha('\n');
+      }
+      }
+}
