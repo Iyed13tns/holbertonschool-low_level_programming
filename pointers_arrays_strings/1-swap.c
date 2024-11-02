8f241d038f8719d1776cf520cf17cc840dc28fb0
Name <m@g.com>
2024-11-02 15:48:40 +0100

    holberton

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - Takes a pointer to an int and updates
  * @a: The first integer to be swapped
- * @b: The second integer to be swapped 
+ * @b: The second integer to be swapped
  *
  * Return: nothing
  */
