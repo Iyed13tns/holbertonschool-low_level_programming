0df9f79c8208926d4da41d969a962c0f31eff92a
Name <m@g.com>
2024-11-02 17:20:37 +0100

    holberton

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -4,12 +4,11 @@
  * reset_to_98 - Takes a pointer to an int and updates
  * @a: The first integer to be swapped
  * @b: The second integer to be swapped
- *
+ * the function that swaps the values of two integers.
  * Return: nothing
  */
 
 void swap_int(int *a, int *b)
-/* the function that swaps the values of two integers. */
 
 {
 int temp = *a;
