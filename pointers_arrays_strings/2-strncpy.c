29685195ec569064552de7a9377923f0d768f60a
Name <m@g.com>
2024-11-03 21:13:48 +0100

    holberton

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,21 +11,21 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-        int a;
+int a;
 
-        a = 0;
-        while (a < n && src[a] != '\0')
-        {
-              dest[a] = src[a];
-              a++;
+a = 0;
+while (a < n && src[a] != '\0')
+{
+dest[a] = src[a];
+a++;
 
-        }
-        while (a < n)
-        {
-              dest[a] = '\0';
-              a++;
-        }
+}
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
 
-        return (dest);
+return (dest);
 
 }
