c7304c65d5c32a42847367c37d0e4caf52fd624f
Name <m@g.com>
2024-11-03 17:55:07 +0100

    holberton

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -13,3 +13,25 @@
  *
  * Return: A pointer to the resulting string dest
  */
+
+char *_strncat(char *dest, char *src, int n)
+
+{
+
+       int i, j;
+
+       i = 0;
+       while (dest[i] != '\0')
+       {
+             i++;
+       }
+       j = 0;
+       while (j < n && src[j] != '\0')
+       {
+       dest[i] = src[j];
+       i++;
+       j++;
+       }
+       dest[i] = '\0';
+       return (dest);
+}
