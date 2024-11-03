09bd145e88e5e2e1abae1ac011329219e112ab5c
Name <m@g.com>
2024-11-03 23:25:38 +0100

    holbeerton

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -8,14 +8,14 @@
  * Return: A pointer to the changed string.
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *str)
 
 {
 int index = 0;
 while (str[index])
 {
 if  (str[index] >= 'a' && str[index] <= '2')
-str[index] -=32;
+str[index] -= 32;
 index++;
         
 }
