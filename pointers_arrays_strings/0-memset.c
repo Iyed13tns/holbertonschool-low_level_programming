ee3e27d5acb296b89d5019d0f062be83d41745be
Name <m@g.com>
2024-11-05 10:11:38 +0100

    holberton

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fill a block of memory with a specific value
+ * @b: the address of memory to print
+ * @s: the size of the memory to print
+ * @b: the desired value
+ * Return: changed array with nex value forr n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+int i = 0;
+
+for (; n > 0; i++)
+{
+s[i] = b;
+n--;
+}
+return (s);
+}
