eeca13688e3dbd426db6561d4444569d629d32d7
Name <m@g.com>
2024-11-05 23:59:31 +0100

    holberton

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,28 +7,28 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-        unsigned int i, n, value, check;
+unsigned int i, n, value, check;
 
-        value = 0;
+value = 0;
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
-                check = 0;
+for (i = 0; s[i] != '\0'; i++)
+{
+check = 0;
 
-                for (n = 0; accept[n] != '\0'; n++)
-                {
+for (n = 0; accept[n] != '\0'; n++)
+{
 
-                        if (accept[n] == s[i])
+if (accept[n] == s[i])
 
-                        {
-                                value++;
-                                check = 1;
-                        }
-                }
+{
+value++;
+check = 1;
+}
+}
 
-                if (check == 0)
-                    break;
-       }
+if (check == 0)
+break;
+}
 
-        return value;
+return value;
 }
