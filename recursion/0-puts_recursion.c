183a9ce01f6a3291b48cf4fd0f2c49ccee959b82
Name <m@g.com>
2024-11-07 19:42:25 +0100

    holberton

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Puts in Recursive Function
+ * @s: char *
+ */
+
+void _puts_recursion(char *s)
+{
+    if (*s == '\0')
+    {
+       printf ('\n')
+    }
+}   return;
