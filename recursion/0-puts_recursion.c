4f8c3f3b8182938ff7239a643348017956b1a2f0
Name <m@g.com>
2024-11-07 19:51:33 +0100

    holberton

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,13 +5,15 @@
  * @s: char *
  */
 
-void _puts_recursion(char *s)
+#include <stdio.h>
+
+void	_puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-       _putchar('\n')
-       return;
-    }
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
