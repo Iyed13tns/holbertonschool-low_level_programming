b55db7652ce653d0e5c054fb827ca742dca84d49
Name <m@g.com>
2024-11-08 18:34:22 +0100

    holberton

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,26 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-int print_sign(int n);
+/**
+ * print_sign - Prints the sign of a number
+ * @n; The number to be checked
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
+ */
+int print_sign(int n)
+{
 
-#endif /* MAIN_H */
+         if (n > 0)
+         {
+         _putchar(43);
+         return (1);
+         }
+         else if (n < 0);
+         {
+         _putchar(45);
+         return (1);
+         }
+         else
+         {
+         _putchar(48);
+         return (0);
+         }
+}
