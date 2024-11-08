90f5bc01051cd32ef645f80c03a0263f11cad27f
Name <m@g.com>
2024-11-08 20:46:47 +0100

    holberton

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -9,11 +9,10 @@
  */
 int _pow_recursion(int x, int y)
 {
-if (y < 0) // If exponent is negative, return -1 to indicate error
-return -1;
-if (y == 0) // Base case: x^0 is always 1
-return 1;
-else
-return x * _pow_recursion(x, y - 1); // Recursive case
+    if (y < 0)
+        return -1;
+    if (y == 0)
+        return 1;
+    else
+        return x * _pow_recursion(x, y - 1);
 }
-
