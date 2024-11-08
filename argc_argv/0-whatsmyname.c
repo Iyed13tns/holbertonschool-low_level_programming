86a6a33d38bfc93b8dc3fde57407d0cfb2da0f78
Name <m@g.com>
2024-11-08 21:55:59 +0100

    holberton

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,12 +2,13 @@
 
 /**
  * main - Prints the name of the program followed by a newline.
+ * @argc: Argument count.
  * @argv: Argument vector (array of arguments).
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-    /* Print the program's name which is stored in argv[0] */
+    (void)argc;  /* Explicitly mark 'argc' as unused */
     printf("%s\n", argv[0]);
     return (0);
 }
