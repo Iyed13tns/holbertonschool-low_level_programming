7ae50e21139ad2ce39e474fb4d936a4fd0302015
Name <m@g.com>
2024-11-08 22:07:20 +0100

    holbertoon

---
+++ argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all arguments it receives.
+ * @argc: Argument count.
+ * @argv: Argument vector (array of arguments).
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+for (int i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);  /* Print each argument on a new line */
+}
+return (0);
+}
