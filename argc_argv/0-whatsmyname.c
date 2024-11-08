fa615fd9ab72466f1fc08eaf38a81715e66cf349
Name <m@g.com>
2024-11-08 21:37:19 +0100

    holberton

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[])
 {
-    /* The program's name is stored in argv[0] */
-    printf("%s\n", argv[0]);
-    return 0;
+/* The program's name is stored in argv[0] */
+printf("%s\n", argv[0]);
+return 0;
 }
