eb8a16c15c4b302a6c9eb464079d746ad75b8e3f
Name <m@g.com>
2024-11-20 11:50:55 +0100

    holberton

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer
+ * @array: is the size of the array
+ * @action: is a pointer to the function you need to use
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+  size x;
+     if (array != NULL && action != NULL)
+     {
+      for (x = 0; x <size ; x++)
+               action (array[x]);
+
+
+     }
+
+
+}
