6b5cd8acda79f58cc92632186f31469662006682
Name <m@g.com>
2024-11-20 10:49:43 +0100

    holberton

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name
+ * @f: fonction qu'il faut print
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+     if(name == NULL || f == NULL)
+          return;
+
+       f(name);
+
+}
