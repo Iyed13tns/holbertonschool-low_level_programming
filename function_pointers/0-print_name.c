db2a41eda054eb159246550d7174348919abc02c
Name <m@g.com>
2024-11-20 10:56:08 +0100

    holberton

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name as is
  * @name: name
  * @f: fonction qu'il faut print
  * Return: Nothing.
