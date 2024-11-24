5ea3ac10aaf1210070daee8a4aa9367dad535c08
Name <m@g.com>
2024-11-24 21:47:26 +0100

    holberton

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,30 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
-
 /**
-  * int_index - ...
-  * @array: ...
-  * @size: ...
-  * @cmp: ...
-  *
-  * Return: ...
-  */
+ * int_index - indexes aray i think
+ * @array: array to go through
+ * @size: size of array
+ * @cmp: function to do something with size
+ * Return: int value
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (size > 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
 	{
-		if (array != NULL && cmp != NULL)
-		{
-			while (i < size)
-			{
-				if (cmp(array[i]))
-					return (i);
-
-				i++;
-			}
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
-
 	return (-1);
 }
