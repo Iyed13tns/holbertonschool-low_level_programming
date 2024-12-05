12f752b4b44b2f573494abf9f3eb92107b50af44
Iyed13tns <m@g.com>
2024-12-05 13:23:08 +0100

    holberton

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,19 +9,19 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        /* Si la chaîne est NULL, afficher [0] (nil) */
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
+while (h != NULL)
+{
 
-        h = h->next;
-        count++;
-    }
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
 
-    return (count);
+h = h->next;
+count++;
+}
+
+return (count);
 }
