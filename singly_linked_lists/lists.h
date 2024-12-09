846ca1adf84eca9d6d7bb7f4ce30ea489e6c0056
Iyed13tns <m@g.com>
2024-12-09 01:19:48 +0100

    hol

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,25 +1,34 @@
-#ifndef LISTS_H
-#define LISTS_H
-
-#include <stddef.h> /* Pour size_t */
+#ifndef SINGLY_LINKED
+#define SINGLY_LINKED
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <string.h>
 
 /**
- * struct list_s - liste simplement chaînée
- * @str: chaîne de caractères (allocée dynamiquement)
- * @len: longueur de la chaîne
- * @next: pointeur vers le nœud suivant
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
  *
- * Description: structure d’un nœud de liste simplement chaînée
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
-/* Prototypes */
 size_t print_list(const list_t *h);
+int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-#endif /* LISTS_H */
 
+
+#endif
