4ea9744a54d702548a7074ae88bc44f33645ae85
Iyed13tns <m@g.com>
2024-12-09 01:21:15 +0100

    holberton

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,14 +1,21 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- *     -
- * @size_t: ...
- * @list_len: ...
- * @list_len: ...
- * Return: ...
+ * list_len - number of elements in a linked list_t list
+ * @h: header of the list
+ * Return: the number of elements
  */
-: size_t list_len(const list_t *h)
 
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		num++;
+		current = current->next;
+	}
+
+	return (num);
+}
